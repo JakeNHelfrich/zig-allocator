f30820ea8e4102957ddf1854699952cd9ccf0aca
Jake Helfrich <jakenichelfrich@gmail.com>
2024-10-29 07:34:51 +0800

    feat: Stubbed Allocator impl

--- src/allocator.zig
+++ src/allocator.zig
@@ -1,26 +1,24 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const failAllocator_vtable = Allocator.VTable{
-    .alloc = failAllocatorAlloc,
-    .resize = Allocator.noResize,
-    .free = Allocator.noFree,
-};
+pub fn StubbedAllocator() type {
+    return struct {
+        pub const vtable: Allocator.VTable = .{
+            .alloc = alloc,
+            .resize = resize,
+            .free = free,
+        };
 
-const fail_allocator = Allocator{
-    .ptr = undefined,
-    .vtable = &failAllocator_vtable,
-};
+        fn alloc(_: *anyopaque, _: usize, _: u8, _: usize) ?[*]u8 {
+            return null;
+        }
 
-fn failAllocatorAlloc(_: *anyopaque, n: usize, log2_alignment: u8, ra: usize) ?[*]u8 {
-    _ = n;
-    _ = log2_alignment;
-    _ = ra;
-    return null;
+        fn resize(_: *anyopaque, _: []u8, _: u8, _: usize, _: usize) bool {
+            return false;
+        }
+
+        fn free(_: *anyopaque, _: []u8, _: u8, _: usize) void {}
+    };
 }
 
-const testing = std.testing;
-test "Allocator basics" {
-    try testing.expectError(error.OutOfMemory, fail_allocator.alloc(u8, 1));
-    try testing.expectError(error.OutOfMemory, fail_allocator.allocSentinel(u8, 1, 0));
-}
+// TODO: Write tests
